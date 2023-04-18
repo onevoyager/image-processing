f77a85bb7a9d1d29715d0609374830e3dc88abcc
onevoyager <130347571+onevoyager@users.noreply.github.com>
2023-04-18 18:25:36 +0530

    update

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,23 +1,22 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  #include CarrierWave::MiniMagick
-  # storage :file
+  include CarrierWave::MiniMagick
+  storage :file
 
   # def store_dir
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
 
-  # version :thumb do
-  #   process resize_to_fit: [200, 200]
-  # end
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
 
   # version :large do
   #   process resize_to_fit: [400, 400]
   # end
 
-  # def extension_whitelist
-  #   %w[jpg jpeg gif png]
-  # end
-  storage :file
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
