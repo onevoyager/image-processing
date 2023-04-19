99d9a441f34efb31a14bdedfe67c22717674e3bc
onevoyager <130347571+onevoyager@users.noreply.github.com>
2023-04-19 09:41:47 +0530

    Add files via upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,17 +2,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
 
-  # def store_dir
-  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  # end
-
   version :thumb do
     process resize_to_fit: [200, 200]
   end
-
-  # version :large do
-  #   process resize_to_fit: [400, 400]
-  # end
 
   def extension_whitelist
     %w[jpg jpeg gif png]
